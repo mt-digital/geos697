276b71b624bb876a7b481fe14cee7aeb7411848d
Matthew Turner <maturner01@gmail.com>
2015-06-12 10:12:15 -0600

    have demo of model ineraction working

--- app/model/views.py
+++ app/model/views.py
@@ -2,19 +2,20 @@
 
 """
 import os
+import json
 
 from flask import render_template, request
-from pandas import read_csv
 from numpy import arange
 from numpy.random import randn
 
 from . import model
 
-MODEL_RUNS = read_csv(os.path.dirname(__file__) + '/../static/model_data.csv')
+MODEL_RUNS = json.loads(open(os.path.dirname(__file__)
+                            + '/../static/model_data.json').read())
+
 # XXX change this to a date when you get a chance-just like this for first go
-DEFAULT_PEAK_INFLOW_DATE = 0.1
-DEFAULT_SNOWPACK_PERCENT_CHANGE = 0.8
-DEFAULT_AG_TO_MUNI_RATIO = 0.3
+DEFAULT_SNOWPACK_PERCENT_CHANGE = "0.8"
+DEFAULT_AG_TO_MUNI_RATIO = "0.30"
 # todo: use data, eg, D_O = MODEL_RUNS[15].downstream_discharge
 DEFAULT_DOWNSTREAM_DISCHARGE = 4.5 + abs(randn(10))*arange(10)
 
@@ -25,27 +26,34 @@ def index():
 
     if request.method == 'POST':
 
-        peak_inflow_date = (request.form['peak-inflow-mo'],
-                            request.form['peak-inflow-day'])
+        peak_inflow_date = (request.form['peak-inflow-rate-mo'],
+                            request.form['peak-inflow-rate-day'])
 
         snowpack_percent_change = request.form['snowpack-percent-change']
         ag_to_muni_ratio = request.form['ag-to-muni-ratio']
 
+        if len(ag_to_muni_ratio.split('.')[-1]) == 1:
+            ag_to_muni_ratio = ag_to_muni_ratio + '0'
+
         downstream_discharge = \
-            MODEL_RUNS[
-                MODEL_RUNS.peak_inflow_date == peak_inflow_date,
-                MODEL_RUNS.snowpack_percent_change == snowpack_percent_change,
-                MODEL_RUNS.ag_to_muni_ratio == ag_to_muni_ratio
-                ].downstream_discharge
+            MODEL_RUNS[','.join([
+                peak_inflow_date[0], peak_inflow_date[1],
+                snowpack_percent_change, ag_to_muni_ratio])]
 
     else:
 
-        peak_inflow_date = DEFAULT_PEAK_INFLOW_DATE
+        month = "6"
+        day = "1"
+        peak_inflow_date = (month, day)
         snowpack_percent_change = DEFAULT_SNOWPACK_PERCENT_CHANGE
         ag_to_muni_ratio = DEFAULT_AG_TO_MUNI_RATIO
-        downstream_discharge = DEFAULT_DOWNSTREAM_DISCHARGE
+        downstream_discharge = \
+            MODEL_RUNS[','.join([month, day,
+                                snowpack_percent_change,
+                                ag_to_muni_ratio])]
 
-    return render_template("model/index.html", peak_inflow_date=peak_inflow_date,
+    return render_template("model/index.html",
+                           peak_inflow_date=peak_inflow_date,
                            snowpack_percent_change=snowpack_percent_change,
                            ag_to_muni_ratio=ag_to_muni_ratio,
                            downstream_discharge=downstream_discharge)
---
+++ app/util.py
@@ -0,0 +1,35 @@
+import scipy
+
+import numpy as np
+#: the water flow always stays above this height (not baseflow b/c of its spec meaning
+BASE_HEIGHT = 100
+START_HEIGHT = 20
+#: total lenght of the "window", a term from signal processing
+WINDOW = 80
+
+def create_fake_hydrograph(peak_inflow_mo=5, peak_inflow_day=1,
+                           percent_snow_change=0.8, ag_to_muni_ratio=0.7):
+    """
+    Create some fake hydrographs that change based on the form inputs
+    """
+    hgraph_peak_loc = peak_inflow_mo*31 + peak_inflow_day + 5
+
+    hgraph_height = BASE_HEIGHT + \
+                    800 * percent_snow_change * (1 - ag_to_muni_ratio)
+
+    hgraph_stddev = 15*percent_snow_change
+
+    len_pre_window = hgraph_peak_loc - WINDOW/2
+
+    pre_steps = np.linspace(START_HEIGHT, BASE_HEIGHT, len_pre_window)
+
+    len_post_window = 365 - (hgraph_peak_loc + WINDOW/2)
+
+    post_steps = np.linspace(BASE_HEIGHT, START_HEIGHT, len_post_window)
+
+    window = (BASE_HEIGHT +
+              hgraph_height * scipy.signal.gaussian(WINDOW, hgraph_stddev))
+
+    hgraph = np.hstack((pre_steps, window, post_steps))
+
+    return hgraph
